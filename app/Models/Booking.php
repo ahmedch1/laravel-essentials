1a70a81a2f99fec3a080a57b33332c675253768e
Ahmed <ahmedchouihi@gmail.com>
2021-06-22 22:43:13 +0100

    'manyToManyRelatiopships'

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -8,10 +8,22 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Booking extends Model
 {
     use SoftDeletes;
-    protected $fillable=['room_id',
+
+    protected $fillable = ['room_id',
         'start',
         'end',
         'is_reservation',
         'is_paid',
         'notes'];
+
+    public function room()
+    {
+        return $this->belongsTo('App\Models\Room');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany('App\Models\User', 'bookings_users',
+            'booking_id', 'user_id')->withTimestamps();
+    }
 }
